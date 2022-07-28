85a37a9d5e32fa92b12d5f887cc1d8853b436f52
HongxiChen1014 <hongxi_chen2020@163.com>
2022-07-28 14:24:34 -0700

    update Product

---
+++ backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -0,0 +1,51 @@
+package com.luv2code.ecommerce.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data // Automatically generate the getter/setter methods behind the scenes
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+
+    @Column(name="sku")
+    private String sku;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name="active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInstock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp // Hibernate will automatically manage the timestamps
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    @CreationTimestamp // Hibernate will automatically manage the timestamps
+    private Date lastUpdated;
+
+}
+
