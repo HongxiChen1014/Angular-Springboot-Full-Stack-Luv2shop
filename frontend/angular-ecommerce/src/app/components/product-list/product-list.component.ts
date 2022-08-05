8e9dd0bc979dbcc4045fcc0502964278a48e8caa
HongxiChen1014 <hongxi_chen2020@163.com>
2022-08-04 22:30:58 -0700

    init luv2shop(add bootstrap & favicon)

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -5,7 +5,8 @@ import { Product } from 'src/app/common/product';
 @Component({
   selector: 'app-product-list',
   // templateUrl: './product-list.component.html',
-  templateUrl: './product-list-table.component.html',
+  // templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
