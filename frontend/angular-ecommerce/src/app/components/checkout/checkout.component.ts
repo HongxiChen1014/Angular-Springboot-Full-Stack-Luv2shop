df0478be573c4051e685d4cceafb99408a7191b7
HongxiChen1014 <hongxi_chen2020@163.com>
2022-08-11 19:08:26 -0700

    update cart totals

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import {FormGroup, FormBuilder, FormControl, Validators} from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
+import { CartService } from 'src/app/services/cart.service';
 import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
 // import { CheckoutService } from 'src/app/services/checkout.service';
 
@@ -27,12 +28,13 @@ shippingAddressStates: State[] = [];
 billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder,
-              private luv2ShopFormService: Luv2ShopFormService
+              private luv2ShopFormService: Luv2ShopFormService,
+              private cartService: CartService
               // private checkoutService: CheckoutService
               ) {}
 
   ngOnInit(): void {
-   // this.reviewCartDetails()
+   this.reviewCartDetails();
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
@@ -167,6 +169,17 @@ billingAddressStates: State[] = [];
     get creditCardNumber() { return this.checkoutFormGroup.get('creditCard.cardNumber'); }
     get creditCardSecurityCode() { return this.checkoutFormGroup.get('creditCard.securityCode'); }
 
+    reviewCartDetails() {
+      // subscribe to cartService.totalQuantity
+      this.cartService.totalQuantity.subscribe(
+        totalQuantity => this.totalQuantity = totalQuantity
+      );
+
+      // subscribe to cartService.totalPrice
+      this.cartService.totalPrice.subscribe(
+        totalPrice => this.totalPrice = totalPrice
+      );
+    }
 
   copyShippingAddressToBillingAddress(event: any) {
 
